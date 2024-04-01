e930191250ef7ecd45a051cd90ddc649b60cf8cf
Anas Monjib <amonjib@gmail.com>
2024-04-01 21:15:29 +0300

    adding __rep__ method and cleaning code

--- todopy/todo.py
+++ todopy/todo.py
@@ -3,23 +3,38 @@ import datetime
 import PyPDF2
 
 class Task:
-    def __init__(self, name, description, importance, category, deadline, frequency):
+    def __init__(self, name, description, importance, category, due_date, 
+                 start_time, duration, frequency):
         self.name = name
         self.description = description
         self.importance = importance
         self.category = category
-        self.deadline = deadline
+        self.due_date = due_date
+        self.start_time = start_time
+        self.duration = duration
         self.frequency = frequency
         self.done = False
+        self.pause = False
+
+    def __repr__(self):
+            return f"Task(title='{self.name}', description='{self.description}
+            ', importance='{self.importance}', due_date='{self.due_date}
+            ', start_time='{self.start_time}', duration='{self.duration}
+            ', frequency='{self.frequency}', category='{self.category}')"
+
 
     def mark_task_done(self):
         self.done = True
 
     def change_deadline(self, deadline):
-        self.deadline = deadline
+        self.due_date = deadline
 
     def change_importance(self, importance):
         self.importance = importance
 
+
+def main():
+     print("Hello World!")
+
 if __name__ == "__main__":
-    print("Hello world")
+     main()
